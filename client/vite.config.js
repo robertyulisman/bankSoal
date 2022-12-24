545cf8742f16e9300967155387b4af50931f66cf
robertyulisman <desainku1092@gmail.com>
2022-12-24 11:12:24 +0700

    next

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,14 +6,29 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
+  // server: {
+  //   host: true,
+  //   port: 3000,
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://localhost:5000",
+  //       changeOrigin: true,
+  //       secure: false,
+  //       ws: true,
+  //       // rewrite: (path) => path.replace(/^\/api/, ""),
+  //     },
+  //   },
+  // },
   server: {
-    host: true,
-    port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://127.0.0.1:5000",
         changeOrigin: true,
+        secure: false,
+        ws: true,
+        // rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
+    port: 3000,
   },
 });
